542ac05be773ad318ade4982cb581522a76aa5ca
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-07-30 23:32:12 +0200

    add index & about urls in main app

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, '')
+
+def about(request):
+    return HttpResponse("<h4>About us page</h4>")
+
+
