775c829384a5ab76b4374d1abe5b9d549aca1476
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-08-07 18:33:56 +0200

    add values on index.html

--- main/views.py
+++ main/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Main page',
+        'values': ['Some', 'Hello', '123'],
+        'obj': {
+            'name': 'Dima',
+            'car': 'BMW',
+            'age': 18,
+            'hobby': 'Football'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
